590c2429ca55511dde91bb829669757300bf7c01
florent claerhout <contact@fclaerhout.fr>
2015-03-25 10:11:23 +0100

    remove utf8 char

--- libcode.py
+++ libcode.py
@@ -129,7 +129,7 @@ class Git(VCS):
 			"log",
 			"--color",
 			"--graph",
-			"--pretty=format:%Cred%h%Creset: %C(yellow)%d%Creset %s %Cgreen — %an %cr%Creset",
+			"--pretty=format:%Cred%h%Creset: %C(yellow)%d%Creset %s %Cgreen - %an %cr%Creset",
 			"--abbrev-commit"))
 
 	def commit(self):
