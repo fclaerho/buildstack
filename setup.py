a16b9fe85193e5c84c09607cdff41da7583674a7
fclaerho <contact@fclaerhout.fr>
2015-07-06 12:53:42 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
 	name = "build", # https://www.python.org/dev/peps/pep-0426/#name
-	version = "2.7.4", # https://www.python.org/dev/peps/pep-0440/
+	version = "2.7.5", # https://www.python.org/dev/peps/pep-0440/
 	packages = ["buildstack"], # https://pythonhosted.org/setuptools/setuptools.html#using-find-packages
 	#description = "",
 	#long_description = "",
@@ -18,10 +18,17 @@ setuptools.setup(
 	install_requires = ["pyutils", "docopt"], # https://packaging.python.org/en/latest/requirements.html#install-requires-vs-requirements-files
 	#package_data = {},
 	#data_files = {},
-	#entry_points = {}, # https://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation
+	entry_points = {
+		"console_scripts": [
+			"build=build:main",
+		],
+	}, # https://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation
 	test_suite = "test",
 	tests_require = ["pyutils", "docopt"],
 	#extra_require = {},
 	#setup_requires = [],
-	dependency_links = ["https://pypi.fclaerhout.fr/simple/pyutils"], # https://pythonhosted.org/setuptools/setuptools.html#dependencies-that-aren-t-in-pypi
+	dependency_links = [
+		"https://pypi.fclaerhout.fr/simple/pyutils",
+		"git+https://github.com/fclaerho/pyutils/tarball/master@v1.2.2#egg=pyutils-1.2.2",
+	], # https://pythonhosted.org/setuptools/setuptools.html#dependencies-that-aren-t-in-pypi
 )
