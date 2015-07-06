22dd44ae2d16ecc20734a0b5977a21ad22249841
fclaerho <contact@fclaerhout.fr>
2015-07-06 18:35:36 +0200

    refactor buildstack package with pyutils

--- buildstack/__init__.py
+++ buildstack/__init__.py
@@ -1,13 +1,4 @@
 
-def get_manifests():
-	import pkgutil
-	manifests = []
-	for loader, modname, _ in pkgutil.iter_modules(__path__):
-		mod = loader.find_module(modname).load_module(modname)
-		if hasattr(mod, "manifest"):
-			if not "name" in mod.manifest:
-				mod.manifest["name"] = mod.__name__
-			manifests.append(mod.manifest)
-	return manifests
+import utils
 
-manifests = get_manifests()
+manifests = utils.get_manifests(__path__)
