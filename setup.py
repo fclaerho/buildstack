b5bc4e1f1c25ada112319408f06c0ca180de9a10
fclaerho <contact@fclaerhout.fr>
2015-07-06 12:10:34 +0200

    fix dependency_links

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
 	name = "build",
 	author = "fclaerhout.fr",
-	version = "2.7.2",
+	version = "2.7.3",
 	license = "MIT",
 	packages = ["buildstack"],
 	test_suite = "test",
@@ -15,5 +15,6 @@ setuptools.setup(
 	entry_points = {"console_scripts": ["build=build:main"]},
 	tests_require = ["pyutils", "docopt"],
 	install_requires = ["pyutils", "docopt"],
-	dependency_links = ["https://pypi.fclaerhout.fr/simple/pyutils"],
+	# Note to self: as pyutils uses build to publish itself, we pull from git instead of pypi.
+	dependency_links = ["git+https://github.com/fclaerho/pyutils.git@v1.2.2#egg=pyutils-1.2.2"],
 )
