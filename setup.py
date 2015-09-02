44a7cc2d3dee2b94734431c46a2e825727989673
fclaerho <contact@fclaerhout.fr>
2015-09-02 10:58:01 +0200

    5.2.0: add templates for stack, ninja, cmake and scons

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
 	name = "buildstack", # https://www.python.org/dev/peps/pep-0426/#name
-	version = "5.1.3", # https://www.python.org/dev/peps/pep-0440/
+	version = "5.2.0", # https://www.python.org/dev/peps/pep-0440/
 	packages = ["buildstack"], # https://pythonhosted.org/setuptools/setuptools.html#using-find-packages
 	#description = "",
 	#long_description = "",
