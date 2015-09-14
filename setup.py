c7b7095569153008fad98a078c289cd36696ffd9
florent claerhout <code@fclaerhout.fr>
2015-09-14 16:25:19 +0200

    5.8.0: add url to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 
 setuptools.setup(
 	name = "buildstack", # https://www.python.org/dev/peps/pep-0426/#name
-	version = "5.7.1", # https://www.python.org/dev/peps/pep-0440/
+	version = "5.8.0", # https://www.python.org/dev/peps/pep-0440/
 	packages = ["buildstack"], # https://pythonhosted.org/setuptools/setuptools.html#using-find-packages
 	#description = "",
 	#long_description = "",
-	#url = "",
+	url = "https://github.com/fclaerho/buildstack",
 	author = "florent claerhout",
 	author_email = "code@fclaerhout.fr",
 	license = "MIT",
@@ -18,7 +18,7 @@ setuptools.setup(
 	install_requires = [
 		"docopt",
 		"PyYAML",
-		"fckit==11.0.2",
+		"fckit",
 	], # https://packaging.python.org/en/latest/requirements.html#install-requires-vs-requirements-files
 	#package_data = {},
 	#data_files = {},
@@ -31,7 +31,7 @@ setuptools.setup(
 	tests_require = [
 		"docopt",
 		"PyYAML",
-		"fckit==11.0.2",
+		"fckit",
 	],
 	#extra_require = {},
 	#setup_requires = [],
