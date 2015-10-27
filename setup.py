2f5d2ae9dc4831dcbd94fd2ddee5727b5479e6a5
Florent Claerhout <code@fclaerhout.fr>
2015-10-27 19:38:44 +0100

    6.2.1: fix setup

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 
 import setuptools
 
+requirements = (
+	"docopt",
+	"PyYAML",
+	"fckit >=14.1.4, <15a0",
+)
+
 setuptools.setup(
 	name = "buildstack", # https://www.python.org/dev/peps/pep-0426/#name
-	version = "6.2.0", # https://www.python.org/dev/peps/pep-0440/
+	version = "6.2.1", # https://www.python.org/dev/peps/pep-0440/
 	packages = ["buildstack"], # https://pythonhosted.org/setuptools/setuptools.html#using-find-packages
 	description = "Build stack wrapper",
 	#long_description = "",
@@ -15,11 +21,7 @@ setuptools.setup(
 	#classifiers = [], # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 	#keyword = [],
 	#py_modules = [],
-	install_requires = [
-		"docopt",
-		"PyYAML",
-		"fckit >=14.1.4, <15a0",
-	], # https://packaging.python.org/en/latest/requirements.html#install-requires-vs-requirements-files
+	install_requires = requirements, # https://packaging.python.org/en/latest/requirements.html#install-requires-vs-requirements-files
 	#package_data = {},
 	#data_files = {},
 	entry_points = {
@@ -28,11 +30,7 @@ setuptools.setup(
 		],
 	}, # https://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation
 	test_suite = "test",
-	tests_require = [
-		"docopt",
-		"PyYAML",
-		"fckit",
-	],
+	tests_require = requirements,
 	#extra_require = {},
 	#setup_requires = [],
 	#dependency_links = [], # https://pythonhosted.org/setuptools/setuptools.html#dependencies-that-aren-t-in-pypi
